7aea64aba4f163202633b4040cc9aebe9a222725
Gaye Rinko <grinko@oa-n3.msk.21-school.ru>
2021-05-24 20:00:37 +0300

    рабил на массив объекто, функция сокращения множителей и делителей

--- validate.js
+++ validate.js
@@ -1,3 +1,11 @@
+include("./distribute.js");
+
+function include(url) {
+	var script = document.createElement('script');
+	script.src = url;
+	document.getElementsByTagName('head')[0].appendChild(script);
+}
+
 var form = document.getElementById("equation_form");
 var output = document.getElementById("output_container");
 
@@ -8,7 +16,7 @@ form.addEventListener('submit', function (oEvent) {
 	oEvent.preventDefault();
 
 
-	if (!inputValid(sEquation))
+	if (!main(sEquation))
 	{
 		console.log("last");
 		return 0;
@@ -26,11 +34,19 @@ function containsSpecialCharactersAndNumbers(str){
 	var regexSymb = /[,!@#\$%&(){}[\]_~`'"\?<>|\\]/i;
 	var re = /=/g;
 	var count = 0;
+	var operatorsFlag = 0;
 
 
 	while (re.test(str))
 		count++;
-	if (regexChar.test(str) || regexSymb.test(str) || count != 1)
+	if (str.includes('++') || str.includes('--') || str.includes('**') || str.includes('//') || str.includes('^^'))
+	{
+		console.log("operators!!!");
+		operatorsFlag++;
+	}
+	if (str[0] == '*' || str[0] == '/')
+		operatorsFlag++;
+	if (regexChar.test(str) || regexSymb.test(str) || count != 1 || operatorsFlag != 0)
 	{
 		console.log("e1");
 		console.log(regexChar.test(str));
@@ -41,10 +57,57 @@ function containsSpecialCharactersAndNumbers(str){
 	return 1;
 }
 
-function inputValid(sInput) {
+function arrangeArray(elements) {
+	var i = 0;
+
+
+	var value;
+
+	console.log("start", elements);
+	while (i < elements.length)
+	{
+		if (elements[i].degree === null)
+		{
+			var tmp = elements[i].sign;
+			if (tmp)
+			{
+				if (tmp == "*")
+				{
+					value = +elements[i-1].k * +elements[i].k;
+					elements[i-1].k = value;
+					elements = elements.filter(item => item !== elements[i]);
+					if (i > 0)
+						i--;
+					continue ;
+				}
+				if (tmp == "/")
+				{
+					value = +elements[i-1].k / +elements[i].k;
+					elements[i-1].k = value;
+					elements = elements.filter(item => item !== elements[i]);
+					if (i > 0)
+						i--;
+					continue ;
+				}
+			}
+			console.log(elements);
+		}
+		i++;
+	}
+	return (elements);
+	// console.log("============");
+	//return elements;
+	
+	// elements = elements.filter(item => item !== value);
+
+	// console.log(elements);
+}
+
+function main(sInput) {
 	//const regexp = new RegExp(['\*,\+,\-\/]? ?\d{0,100}\.?\d{0,100} ?[\*,\+,\-\/]? ?[x,X]\^\d?', '');
 	sInput = sInput.replaceAll(' ', '');
 	const re = /[\*,\+,\-\/]?\d{0,100}\.?\d{0,100}?[\*,\+,\-\/]?[x,X]\^\d?/g;
+	var elements = [];
 
 	let result = "";
 	var count = 0;
@@ -53,9 +116,12 @@ function inputValid(sInput) {
 		return 0;
 	
 	sInput = stepeny(sInput);
-	distributeToArray(sInput);
+	elements = distributeToArray(sInput);
+	var test = elements;
+	elements = arrangeArray(elements);
+	console.log("last ", elements);
 	sInput = moveLeftBehindEqual(sInput);
-	console.log(sInput);
+	//console.log(sInput);
 	// if (!testy(sInput))
 	// 	return 0;
 
@@ -64,106 +130,17 @@ function inputValid(sInput) {
 	return 1;
 }
 
-function distributeToArray(sInput)
-{
-	const re = /[\*,\+,\-\/]?\d{0,100}\.?\d{0,100}?[\*]?[x,X]\^\d?/g;
-	var result;
-	var splits = sInput.split(/[\+,\-\=]/);
-	var elements = [];
-	var elements2 = [];
-	var len = 0;
-	var k = 0;
-	var degree = 0;
-	var sign = '';
-	var tmp;
-	var i = 0;
-	var element = {
-		sign:null,
-		k:null,
-		degree:null
-	}
-
-	var test = sInput;
-	while (result = re.exec(sInput)) {
-		k = null;
-		sign = null;
-		result = result[0];
-		len = result.length - 1;
-		degree = result[len];
-		if (/[\*]/.test(result))
-		{
-			tmp = result.split('*')[0];
-			if (/[\*,\+,\-\/]/.test(tmp))
-			{
-				sign = tmp[0];
-				k = tmp.slice(1);
-			}
-			else
-				k = tmp;
-		}
-		element = {
-			sign:sign,
-			k:k,
-			degree:degree
-		}
-		elements.push(element);
-		test = test.replace(result, 'p');
-	}
-	console.log(test);
-	//var reg = /[\*\+\-\/]?\d{0,100}\.?\d{0,100}/g;
-	var reg = new RegExp(/[\*\+\-\/]?\d{0,100}\.?\d{0,100}?/g);
-	var res = "";
-	while (res = reg.exec(test)) {
-		console.log("I1" + res.index);
-		console.log("I2" + reg.lastIndex);
-		if (res.index === reg.lastIndex)
-			reg.lastIndex++;
-		res = res[0];
-		console.log(res);
-		if (res)
-		{
-			if (/[\+\-\*\/]/.test(res))
-			{
-				//console.log("yes");
-				sign = res[0];
-				k = res.slice(1);
-			}
-			else
-			{
-				//console.log("no");
-				sign = null;
-				k = res;
-			}
-			element = {
-				sign:sign,
-				k:k,
-				degree:null
-			}
-			elements2.push(element);
-			test = test.replace(res, 'l');
-		}
-	}
-	console.log(test);
-	//console.log(test.split('p'));
-	// while (i < splits.length)
-	// {
-	// 	console.log(splits[i]);
-	// 	i++;
-	// }
-	
-	console.log(elements2);
-}
-
 function moveLeftBehindEqual(sInput){
-
+	
 }
 
-function printStep(str)
+function printStep(str, flag)
 {
 	const fragment = document.createDocumentFragment();
 
 	const step = document.createElement('p');
-	// step.classList.add('b_card-text');
+	if (flag == 1)
+		step.classList.add('symbols');
 	step.textContent = str;
 
 	fragment.appendChild(step);
@@ -191,98 +168,98 @@ function stepeny(sInput) {
 				sInput = sInput.replace(sInput, '1');
 		}
 	}
-	printStep("Преобразование: значений выражения, содержащего степени"); //упростить выражение
-	printStep(sInput);
+	printStep("Преобразование: значений выражения, содержащего степени", 0); //упростить выражение
+	printStep(sInput, 1);
 	return(sInput);
 }
 
-function testy(str) {
-	var splits = str.split(/['\+,\-,\=]/);
-	//var operators = str.split(/\d{0,100}\.?\d{0,100}?[\*,\+,\-\/]?[x,X]\^\d/);
-	var operators = str.split(/[\+,\-]?\d{0,100}\.?\d{0,100}?[\*,\+,\-\/]?[x,X]\^\d/);
-	//console.log(operators);
-	var step = "";
-	var n = 0;
-	var i = 0;
-	var elements = [];
+// function testy(str) {
+// 	var splits = str.split(/['\+,\-,\=]/);
+// 	//var operators = str.split(/\d{0,100}\.?\d{0,100}?[\*,\+,\-\/]?[x,X]\^\d/);
+// 	var operators = str.split(/[\+,\-]?\d{0,100}\.?\d{0,100}?[\*,\+,\-\/]?[x,X]\^\d/);
+// 	//console.log(operators);
+// 	var step = "";
+// 	var n = 0;
+// 	var i = 0;
+// 	var elements = [];
 
 
-	if (splits.length != operators.length - 1)
-	{
-		console.log("e3");
-		console.log(splits);
-		console.log(operators);
-		printError("Incorrect Input");
-		return 0;
-	}
-//	while (n < operators.length)
-//	{
-//		if (operators[n] == '=')
-//			operators.splice(n, 1);
-//		n++;
-//	}
-	while (i < splits.length)
-	{
-		var len = 0;
-		var k = 0;
-		var degree = 0;
-		var sign = '';
-		var element = {
-			sign:null,
-			k:null,
-			degree:null
-		}
+// 	if (splits.length != operators.length - 1)
+// 	{
+// 		console.log("e3");
+// 		console.log(splits);
+// 		console.log(operators);
+// 		printError("Incorrect Input");
+// 		return 0;
+// 	}
+// //	while (n < operators.length)
+// //	{
+// //		if (operators[n] == '=')
+// //			operators.splice(n, 1);
+// //		n++;
+// //	}
+// 	while (i < splits.length)
+// 	{
+// 		var len = 0;
+// 		var k = 0;
+// 		var degree = 0;
+// 		var sign = '';
+// 		var element = {
+// 			sign:null,
+// 			k:null,
+// 			degree:null
+// 		}
 
-		if (/[x,X]\^\d/.test(splits[i]))
-		{
-			len = splits[i].length;
-			k = splits[i].split('*')[0];
-			degree = splits[i][len - 1];
-			sign = splits[i][0];
-			if (degree == 0)
-				splits[i] = k;
-			element = {
-				sign:operators[n],
-				k:k,
-				degree:degree
-			}
-			elements.push(element);
-		}
-		else
-		{
-			if (operators[n + 1] == '=')
-			{
-				element = {
-					sign:operators[n + 1],
-					k:splits[i],
-					degree:null
-				}
-			}
-			else
-			{
-				element = {
-					sign:operators[n],
-					k:splits[i],
-					degree:null
-				}
-			}
-			elements.push(element);
-		}
+// 		if (/[x,X]\^\d/.test(splits[i]))
+// 		{
+// 			len = splits[i].length;
+// 			k = splits[i].split('*')[0];
+// 			degree = splits[i][len - 1];
+// 			sign = splits[i][0];
+// 			if (degree == 0)
+// 				splits[i] = k;
+// 			element = {
+// 				sign:operators[n],
+// 				k:k,
+// 				degree:degree
+// 			}
+// 			elements.push(element);
+// 		}
+// 		else
+// 		{
+// 			if (operators[n + 1] == '=')
+// 			{
+// 				element = {
+// 					sign:operators[n + 1],
+// 					k:splits[i],
+// 					degree:null
+// 				}
+// 			}
+// 			else
+// 			{
+// 				element = {
+// 					sign:operators[n],
+// 					k:splits[i],
+// 					degree:null
+// 				}
+// 			}
+// 			elements.push(element);
+// 		}
 		
-		// console.log(operators[i]);
-		// console.log(splits[i]);
-		if (operators[n + 1] == '=')
-		{
-			step += operators[n] + splits[i] + operators[n + 1];
-			n++;
-		}
-		else
-			step += operators[n] + splits[i];
-		i++;
-		n++;
-	}
-	console.log(elements)
-	printStep("Преобразование: значений выражения, содержащего степени"); //упростить выражение
-	printStep(step);
-	return 1;
-}
+// 		// console.log(operators[i]);
+// 		// console.log(splits[i]);
+// 		if (operators[n + 1] == '=')
+// 		{
+// 			step += operators[n] + splits[i] + operators[n + 1];
+// 			n++;
+// 		}
+// 		else
+// 			step += operators[n] + splits[i];
+// 		i++;
+// 		n++;
+// 	}
+// 	console.log(elements)
+// 	printStep("Преобразование: значений выражения, содержащего степени"); //упростить выражение classify 
+// 	printStep(step);
+// 	return 1;
+// }
